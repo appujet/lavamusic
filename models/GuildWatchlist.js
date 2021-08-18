f5504d7d0553977f2c36854947d72e17c132e1f1
S Dip <77108939+brblacky@users.noreply.github.com>
2021-08-18 12:23:51 +0530

    Delete models directory

--- models/GuildWatchlist.js
+++
@@ -1,9 +0,0 @@
-const { model, Schema } = require('mongoose');
-
-module.exports = model('server_watchlists', Schema({
-  _id: String,
-  channelID: { type: String, default: null },
-  data: { type: Array, default: []}
-},{
-  versionKey: false
-}));
