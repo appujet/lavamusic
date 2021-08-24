e55367339b4ceb0e64cee321a5a528164c9b7c37
S Dip <77108939+brblacky@users.noreply.github.com>
2021-08-24 22:14:24 +0530

    Fixed 247 command

--- commands/Music/247.js
+++ commands/Music/247.js
@@ -1,41 +1,34 @@
 const { MessageEmbed } = require("discord.js");
 module.exports = {
-	name: "247",
-    aliases: ["24h", "24/7", "24*7"],
-    category: "Music",
-    description: "24/7 in voice channel",
-    args: false,
-    usage: "",
-    permission: [],
-    owner: false,
-    player: false,
-    inVoiceChannel: true,
-    sameVoiceChannel: false,
-	 execute: async (message, args, client, prefix) => {
-    
-  const player = message.client.manager.players.get(message.guild.id);
-		if (!player) {
-	const embed = new MessageEmbed()
-		.setDescription('there is nothing playing')
-		.setColor(client.embedColor)
-		return message.channel.send({embeds: [embed]})
-  const { channel } = message.member.voice
+  name: "247",
+  aliases: ["24h", "24/7", "24*7"],
+  category: "Music",
+  description: "24/7 in voice channel",
+  args: false,
+  usage: "",
+  permission: [],
+  owner: false,
+  player: true,
+  inVoiceChannel: true,
+  sameVoiceChannel: true,
+  execute: async (message, args, client, prefix) => {
 
-		if (player.twentyFourSeven) {
-			player.twentyFourSeven = false;
-			 const embed = new MessageEmbed()
-        .setDescription("24/7 mode is **disabled**")
-        .setColor(client.embedColor)
-			return message.channel.send({embeds: [embed]})
-		} else {
-			player.twentyFourSeven = true;
-		 const embed = new MessageEmbed()
-        .setDescription("24/7 mode is **Enable**")
-        .setColor(client.embedColor)
-			return message.channel.send({embeds: [embed]})
-		  
-		}
 
+    const player = message.client.manager.players.get(message.guild.id);
+    if (player.twentyFourSeven) {
+      player.twentyFourSeven = false;
+      const embed = new MessageEmbed()
+       .setColor(client.embedColor)
+       .setDescription(`24/7 mode is now off.`)
+      return message.channel.send({embeds: [embed]});
+    }
+    else {
+      player.twentyFourSeven = true;
+      const embed = new MessageEmbed()
+       .setColor(client.embedColor)
+       .setDescription(`24/7 mode is now on.`)
+      
+      return message.channel.send({embeds: [embed]});
+    }
   }
-}
-}
+};
