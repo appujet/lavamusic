07c1d4b06e5ceb2b6479aaf127bcae0bc0c41ef8
S Dip <77108939+brblacky@users.noreply.github.com>
2021-09-21 08:25:17 +0530

    Create node.js

---
+++ slashCommands/Information/node.js
@@ -0,0 +1,39 @@
+const { MessageEmbed, CommandInteraction, Client } = require("discord.js")
+
+module.exports = {
+    name: "node",
+    description: "check lavalink server information",
+
+    /**
+     * 
+     * @param {Client} client 
+     * @param {CommandInteraction} interaction 
+     */
+
+    run: async (client, interaction) => {
+        await interaction.deferReply({
+          ephemeral: false
+        });
+     const all = client.manager.nodes.map(node => 
+            `Node ${(node.options.identifier)} Connected` +
+            `\nPlayer: ${node.stats.players}` +
+            `\nPlaying Players: ${node.stats.playingPlayers}` +
+            `\nUptime: ${new Date(node.stats.uptime).toISOString().slice(11, 19)}` +
+            `\n\nMemory` +
+            `\nReservable Memory: ${Math.round(node.stats.memory.reservable / 1024 / 1024)}mb` +
+            `\nUsed Memory: ${Math.round(node.stats.memory.used / 1024 / 1024)}mb` +
+            `\nFree Memory: ${Math.round(node.stats.memory.free / 1024 / 1024)}mb` +
+            `\nAllocated Memory: ${Math.round(node.stats.memory.allocated / 1024 / 1024)}mb` +
+            "\n\nCPU" +
+            `\nCores: ${node.stats.cpu.cores}` +
+            `\nSystem Load: ${(Math.round(node.stats.cpu.systemLoad * 100) / 100).toFixed(2)}%` +
+            `\nLavalink Load: ${(Math.round(node.stats.cpu.lavalinkLoad * 100) / 100).toFixed(2)}%`
+        ).join('\n\n----------------------------\n');
+
+        const embed = new MessageEmbed()
+            .setAuthor('Lavalink Node', client.user.displayAvatarURL())
+            .setDescription(`\`\`\`${all}\`\`\``)
+            .setColor(client.embedColor)
+        await interaction.followUp({embeds: [embed]})
+    }
+}
