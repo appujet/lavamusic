b651a77a3bb572490728b87bbabc675995cd9786
cortex <75237415+Stranger165@users.noreply.github.com>
2021-09-30 19:42:12 +0530

    🪐Support mongoose latest version

--- handler/Client.js
+++ handler/Client.js
@@ -38,25 +38,23 @@ module.exports = async (client) => {
      */
     
     const dbOptions = {
-      useNewUrlParser: true,
-      autoIndex: false,
-      poolSize: 5,
-      connectTimeoutMS: 10000,
-      family: 4,
-      useUnifiedTopology: true,
-    };
-      mongoose.connect(client.config.mongourl, dbOptions);
-      mongoose.set("useFindAndModify", false);
-      mongoose.Promise = global.Promise;
-        mongoose.connection.on('connected', () => {
-            client.logger.log('[DB] DATABASE CONNECTED', "ready");
-            });
-        mongoose.connection.on('err', (err) => {
-                console.log(`Mongoose connection error: \n ${err.stack}`, "error");
-            });
-        mongoose.connection.on('disconnected', () => {
-                console.log('Mongoose disconnected');
-            });
+        useNewUrlParser: true,
+        autoIndex: false,
+        connectTimeoutMS: 10000,
+        family: 4,
+        useUnifiedTopology: true,
+      };
+        mongoose.connect(client.config.mongourl, dbOptions);
+        mongoose.Promise = global.Promise;
+          mongoose.connection.on('connected', () => {
+              client.logger.log('[DB] DATABASE CONNECTED', "ready");
+              });
+          mongoose.connection.on('err', (err) => {
+                  console.log(`Mongoose connection error: \n ${err.stack}`, "error");
+              });
+          mongoose.connection.on('disconnected', () => {
+                  console.log('Mongoose disconnected');
+              });
         
     /**
      * Error Handler
