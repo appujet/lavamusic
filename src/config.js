316990a318101664962c552b0a07b1454dbef25c
S Dip <77108939+brblacky@users.noreply.github.com>
2021-11-16 14:56:21 +0000

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    token: process.env.TOKEN || "ODkyNjMyMTM1MDk2MjA5NDI4.YVPudQ.4H46V2RRtYdvQdbc78dhA7N4pxk", 
+    token: process.env.TOKEN || "", 
     prefix: process.env.PREFIX || "!",
     ownerID: process.env.OWNERID || "491577179495333903",
-    SpotifyID: process.env.SPOTIFYID || "69ebbd15cba9474a9d46e5aa95733b15",
-    SpotifySecret: process.env.SPOTIFYSECRET || "185da21de3904b7db61d4d12c455c166",
-    mongourl: process.env.MONGO_URI || "mongodb+srv://blacky:testmongo@cluster0.ssqzd.mongodb.net/test",
+    SpotifyID: process.env.SPOTIFYID || "",
+    SpotifySecret: process.env.SPOTIFYSECRET || "",
+    mongourl: process.env.MONGO_URI || "",
     embedColor: process.env.COlOR || "#303236",
     logs: process.env.LOGS || "",
 
