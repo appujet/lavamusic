86d989d235e61f16763ea98c73d91d2c799fbe57
S Dip <77108939+brblacky@users.noreply.github.com>
2021-11-16 20:41:02 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    token: process.env.TOKEN || "", 
-    prefix: process.env.PREFIX || "!",
-    ownerID: process.env.OWNERID || "491577179495333903",
-    SpotifyID: process.env.SPOTIFYID || "",
-    SpotifySecret: process.env.SPOTIFYSECRET || "",
-    mongourl: process.env.MONGO_URI || "",
-    embedColor: process.env.COlOR || "#303236",
-    logs: process.env.LOGS || "",
+    token: process.env.TOKEN || "",  // your bot token
+    prefix: process.env.PREFIX || "!", // bot prefix
+    ownerID: process.env.OWNERID || "491577179495333903", //your discord id
+    SpotifyID: process.env.SPOTIFYID || "", // spotify client id
+    SpotifySecret: process.env.SPOTIFYSECRET || "", // spotify client secret
+    mongourl: process.env.MONGO_URI || "", // MongoDb URL
+    embedColor: process.env.COlOR || "#303236", // embed colour
+    logs: process.env.LOGS || "", // guild create and delete logs
 
   nodes: {
      
