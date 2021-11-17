8995de8bf82fe89e69963988a4d3b41068b75fdb
S Dip <77108939+brblacky@users.noreply.github.com>
2021-11-17 18:53:43 +0530

    Update progressbar.js

--- src/utils/progressbar.js
+++ src/utils/progressbar.js
@@ -1,17 +1,15 @@
 module.exports = {
-    progressbar: function (total, current, size, line, slider) {
-        if (current > total) {
-            const bar = line.repeat(size + 2);
-            return bar;
-        } else {
-            const percentage = current / total;
-            const progress = Math.round((size * percentage));
-            const emptyProgress = size - progress;
-            const progressText = line.repeat(progress).replace(/.$/, slider);
-            const emptyProgressText = line.repeat(emptyProgress);
-            const bar = progressText + emptyProgressText;
-            return bar;
-        }
+    progressbar: function (player) {
+    let size = 25;
+    let line = "▬";
+    let slider = "🔘";
+    
+    if (!player.queue.current) return `${slider}${line.repeat(size - 1)}]`;
+    let current = player.queue.current.duration !== 0 ? player.position : player.queue.current.duration;
+    let total = player.queue.current.duration;
+    let bar = current > total ? [line.repeat(size / 2 * 2), (current / total) * 100] : [line.repeat(Math.round(size / 2 * (current / total))).replace(/.$/, slider) + line.repeat(size - Math.round(size * (current / total)) + 1), current / total];
+   
+    if (!String(bar).includes(slider)) return `${slider}${line.repeat(size - 1)}`;
+    return `${bar[0]}`;
     }
-
-																				 }
+ }
