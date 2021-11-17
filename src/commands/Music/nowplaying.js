14f0fc39828fc898b171a34bace807bbe78bc330
S Dip <77108939+brblacky@users.noreply.github.com>
2021-11-17 15:48:43 +0530

    Update nowplaying.js

--- src/commands/Music/nowplaying.js
+++ src/commands/Music/nowplaying.js
@@ -37,7 +37,7 @@ module.exports = {
         var slider = '🔘';
 
         let embed = new MessageEmbed()
-            .setDescription(`${emojimusic} **Now Playing**\n[${song.title}](${song.uri}) - \`[${convertTime(song.duration)}]\` [<@${song.requester.id}>]`)
+            .setDescription(`${emojimusic} **Now Playing**\n[${song.title}](${song.uri}) - \`[${convertTime(song.duration)}]\` [${song.requester}]`)
             .setThumbnail(song.displayThumbnail("3"))
             .setColor(message.client.embedColor)
             .addField("\u200b", progressbar(total, current, size, line, slider))
