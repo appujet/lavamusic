753b154fbd8da55852501c7a9c7881c557253ce1
S Dip <77108939+brblacky@users.noreply.github.com>
2021-11-17 18:49:10 +0530

    Update nowplaying.js

--- src/commands/Music/nowplaying.js
+++ src/commands/Music/nowplaying.js
@@ -3,7 +3,7 @@ const { convertTime } = require('../../utils/convert.js');
 const { progressbar } = require('../../utils/progressbar.js')
 
 module.exports = {
-    name: "nowplaying",
+  	name: "nowplaying",
     aliases: ["np"],
     category: "Music",
     description: "Show now playing song",
@@ -14,17 +14,21 @@ module.exports = {
     player: true,
     inVoiceChannel: false,
     sameVoiceChannel: false,
- execute: async (message, args, client, prefix) => {
+	 execute: async (message, args, client, prefix) => {
   
         const player = message.client.manager.get(message.guild.id);
+
         if (!player.queue.current) {
             let thing = new MessageEmbed()
                 .setColor("RED")
                 .setDescription("There is no music playing.");
             return message.channel.send(thing);
         }
+
         const song = player.queue.current
         const emojimusic = client.emoji.music;
+        var total = song.duration;
+        var current = player.position;
 
         let embed = new MessageEmbed()
             .setDescription(`${emojimusic} **Now Playing**\n[${song.title}](${song.uri}) - \`[${convertTime(song.duration)}]\`- [${song.requester}] \n\n\`${progressba(player)}\``)
