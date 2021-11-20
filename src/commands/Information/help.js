4175f53420da4254625487c80856e36f680efc5b
S Dip <77108939+brblacky@users.noreply.github.com>
2021-11-20 20:51:05 +0530

    Update help.js

--- src/commands/Information/help.js
+++ src/commands/Information/help.js
@@ -39,7 +39,7 @@ module.exports = {
      b.reply({ ephemeral: true, content: `Only **${message.author.tag}** can use this button, if you want then you've to run the command again.`}); return false;
            };
       },
-      time : 200000
+      time : 60000,
       idle: 60000/2
     });
     collector.on("end", async () => {
