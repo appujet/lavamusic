f8641cc06f4301545c491f4a03e8e5a249599b75
S Dip <77108939+brblacky@users.noreply.github.com>
2021-11-20 21:02:58 +0530

    Update help.js

--- src/slashCommands/Information/help.js
+++ src/slashCommands/Information/help.js
@@ -33,9 +33,9 @@ module.exports = {
      let _commands;
      let editEmbed = new MessageEmbed();
      
-    const m = await interaction.editReply({ embeds: [embed], components: [new MessageActionRow().addComponents(but1, but2, but3, but4)] });
+     await interaction.editReply({ embeds: [embed], components: [new MessageActionRow().addComponents(but1, but2, but3, but4)] });
 
-    const collector = m.createMessageComponentCollector({
+    const collector = interaction.channel.createMessageComponentCollector({
       filter: (b) => {
       if(b.user.id === interaction.member.user.id) return true;
        else {
@@ -46,29 +46,27 @@ module.exports = {
       idle: 60000/2
     });
     collector.on("end", async () => {
-	 if(!m) return;
-        await m.editReply({ components: [new MessageActionRow().addComponents(but1.setDisabled(true), but2.setDisabled(true), but3.setDisabled(true), but4.setDisabled(true))] }).catch(() => {});
+        await interaction.editReply({ components: [new MessageActionRow().addComponents(but1.setDisabled(true), but2.setDisabled(true), but3.setDisabled(true), but4.setDisabled(true))] }).catch(() => {});
      });
     collector.on('collect', async (b) => {
         if(b.customId === "home") {
-           if(!m) return;
-           return await m.edit({ embeds: [embed], components: [new MessageActionRow().addComponents(but1, but2, but3, but4)] })
+           return await interaction.editReply({ embeds: [embed], components: [new MessageActionRow().addComponents(but1, but2, but3, but4)] })
         }
         if(b.customId === "music") {
          _commands = client.commands.filter((x) => x.category && x.category === "Music").map((x) => `\`${x.name}\``);
              editEmbed.setColor(client.embedColor).setDescription(_commands.join(", ")).setTitle("Music Commands").setFooter(`Total ${_commands.length} music commands.`);
-           if(!m) return;
-           return await m.edit({ embeds: [editEmbed], components: [new MessageActionRow().addComponents(but1, but2, but3, but4)] })
+ 
+           return await interaction.editReply({ embeds: [editEmbed], components: [new MessageActionRow().addComponents(but1, but2, but3, but4)] })
         }
          if(b.customId == "info") {
          _commands = client.commands.filter((x) => x.category && x.category === "Information").map((x) => `\`${x.name}\``);
              editEmbed.setColor(client.embedColor).setDescription(_commands.join(", ")).setTitle("Information Commands").setFooter(`Total ${_commands.length} Information commands.`)
-          return await m.edit({ embeds: [editEmbed], components: [new MessageActionRow().addComponents(but1, but2, but3, but4)] })
+          return await interaction.editReply({ embeds: [editEmbed], components: [new MessageActionRow().addComponents(but1, but2, but3, but4)] })
          }
          if(b.customId == "config") {
          _commands = client.commands.filter((x) => x.category && x.category === "Config").map((x) => `\`${x.name}\``);
              editEmbed.setColor(client.embedColor).setDescription(_commands.join(", ")).setTitle("Config Commands").setFooter(`Total ${_commands.length} Config commands.`)
-          return await m.edit({ embeds: [editEmbed], components: [new MessageActionRow().addComponents(but1, but2, but3, but4)] })
+          return await interaction.editReply({ embeds: [editEmbed], components: [new MessageActionRow().addComponents(but1, but2, but3, but4)] })
          
         }
      });
