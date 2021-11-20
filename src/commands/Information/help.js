c65f9de2070af6be1c4f359745088fffb2fe1ecf
S Dip <77108939+brblacky@users.noreply.github.com>
2021-11-20 19:54:41 +0530

    Update help.js

--- src/commands/Information/help.js
+++ src/commands/Information/help.js
@@ -1,13 +1,7 @@
-const {
-    MessageEmbed,
-    Message,
-    Client
-} = require("discord.js");
-const { readdirSync } = require("fs");
-const create_mh = require(`../../utils/menu.js`);
+const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
 
 module.exports = {
-    name: "help",
+    name: "help2",
     category: "Information",
     aliases: [ "h" ],
     description: "Return all commands, or one specific command",
@@ -15,273 +9,60 @@ module.exports = {
     usage: "",
     permission: [],
     owner: false,
-   execute: async (message, args, client, prefix) => {
+ execute: async (message, args, client, prefix) => {
 
-     let color = client.embedColor;
-      let categories = [];
-        let cots = [];
+  const embed = new MessageEmbed()
+    .setTitle(`${client.user.username} Help`)
+    .setDescription(` Hello **<@${message.author.id}>**, I am <@${client.user.id}>.  \n\nA Discord Music Bot With Many Awesome Features, \nSupport Many Sources \n\n\`🎵\`•Music\n\`ℹ️\`•information\n\`⚙️\`•Config\n\n *Choose an category below button to see commands* \n\n`)
+    .setThumbnail(client.user.displayAvatarURL())
+    .setColor(client.embedColor)
+    .setTimestamp()
+    .setFooter(`Requested by: ${message.author.tag}`)
 
-        if (!args[0]) {
+    let but1 = new MessageButton().setCustomId("home").setLabel("Home").setStyle("SUCCESS")
+  
+    let but2 = new MessageButton().setCustomId("music").setLabel("Music").setStyle("PRIMARY")
+  
+    let but3 = new MessageButton().setCustomId("info").setLabel("Info").setStyle("PRIMARY");
 
-            //categories to ignore
-            let ignored = [
-                "Owner"
-            ];
+    let but4 = new MessageButton().setCustomId("config").setLabel("Config").setStyle("PRIMARY");
 
-            const emo = {
+     let _commands;
+     let editEmbed = new MessageEmbed();
+     
+    const m = await message.reply({ embeds: [embed], components: [new MessageActionRow().addComponents(but1, but2, but3, but4)] });
 
-        Config: "⚙️",
-        Information: "ℹ️",
-        Music: "🎵",
-        Owner: "👑" 
-            }
-
-            let ccate = [];
-           readdirSync("./src/commands/").forEach((dir) => {
-                if (ignored.includes(dir.toLowerCase())) return;
-                const commands = readdirSync(`./src/commands/${dir}/`).filter((file) =>
-                    file.endsWith(".js")
-                );
-
-                if (ignored.includes(dir.toLowerCase())) return;
-
-                const name = `${emo[dir]} - ${dir}`;
-                
-                let nome = dir.toUpperCase();
-
-                let cats = new Object();
-
-                //this is how it will be created as
-                cats = {
-                    name: name,
-                    value: `\`${prefix}help ${dir.toLowerCase()}\``,
-                    inline: true
-                }
-
-
-                categories.push(cats);
-                ccate.push(nome);
-            });
-            //embed
-            const embed = new MessageEmbed()
-                .setTitle(`Bot Commands`)
-                .setDescription(`>>> My prefix is \`${prefix}\`\nUse the menu, or use \`${prefix}help [category]\` to view commands base on their category!`)
-                .addFields(categories)
-                .setFooter(
-                    `Requested by ${message.author.tag}`,
-                    message.author.displayAvatarURL({
-                        dynamic: true
-                    })
-                )
-                .setTimestamp()
-                .setColor(color)
-            let menus = create_mh(ccate);
-            return message.channel.send({
-                embeds: [embed],
-                components: menus.smenu
-            }).then((msgg) => {
-
-                const menuID = menus.sid;
-
-                const select = async (interaction) => {
-                    if (interaction.customId != menuID) return;
-
-                    let {
-                        values
-                    } = interaction;
-
-                    let value = values[0];
-
-                    let catts = [];
-
-                    readdirSync("./src/commands/").forEach((dir) => {
-                        if (dir.toLowerCase() !== value.toLowerCase()) return;
-                        const commands = readdirSync(`./src/commands/${dir}/`).filter((file) =>
-                            file.endsWith(".js")
-                        );
-
-
-                        const cmds = commands.map((command) => {
-                            let file = require(`../../commands/${dir}/${command}`); //getting the commands again
-
-                            if (!file.name) return "No command name.";
-
-                            let name = file.name.replace(".js", "");
-
-                            if (client.commands.get(name).hidden) return;
-
-
-                            let des = client.commands.get(name).description;
-                            let emo = client.commands.get(name).emoji;
-                            let emoe = emo ? `${emo} - ` : ``;
-
-                            let obj = {
-                                cname: `${emoe}\`${name}\``,
-                                des
-                            }
-
-                            return obj;
-                        });
-
-                        let dota = new Object();
-
-                        cmds.map(co => {
-                            if (co == undefined) return;
-
-                            dota = {
-                                name: `${cmds.length === 0 ? "In progress." : co.cname}`,
-                                value: co.des ? co.des : `No Description`,
-                                inline: true,
-                            }
-                            catts.push(dota)
-                        });
-
-                        cots.push(dir.toLowerCase());
-                    });
-
-                    if (cots.includes(value.toLowerCase())) {
-                        const combed = new MessageEmbed()
-                            .setTitle(`__${value.charAt(0).toUpperCase() + value.slice(1)} Commands!__`)
-                            .setDescription(`Use \`${prefix}help\` followed by a command name to get more information on a command.\nFor example: \`${prefix}help ping\`.\n\n`)
-                            .addFields(catts)
-                            .setColor(color)
-
-                        await interaction.deferUpdate();
-
-                        return interaction.message.edit({
-                            embeds: [combed],
-                            components: menus.smenu
-                        })
-                    };
-
-                };
-
-                const filter = (interaction) => {
-                    return !interaction.user.bot && interaction.user.id == message.author.id
-                };
-
-                const collector = msgg.createMessageComponentCollector({
-                    filter,
-                    componentType: "SELECT_MENU"
-                });
-                collector.on("collect", select);
-                collector.on("end", () => null);
-
-            });
-
-        } else {
-            let catts = [];
-
-            readdirSync("src/commands").forEach((dir) => {
-                if (dir.toLowerCase() !== args[0].toLowerCase()) return;
-                const commands = readdirSync(`src//commands/${dir}/`).filter((file) =>
-                    file.endsWith(".js")
-                );
-
-
-                const cmds = commands.map((command) => {
-                    let file = require(`../../commands/${dir}/${command}`);
-
-                    if (!file.name) return "No command name.";
-
-                    let name = file.name.replace(".js", "");
-
-                    if (client.commands.get(name).hidden) return;
-
-
-                    let des = client.commands.get(name).description;
-                    let emo = client.commands.get(name).emoji;
-                    let emoe = emo ? `${emo} - ` : ``;
-
-                    let obj = {
-                        cname: `${emoe}\`${name}\``,
-                        des
-                    }
-
-                    return obj;
-                });
-
-                let dota = new Object();
-
-                cmds.map(co => {
-                    if (co == undefined) return;
-
-                    dota = {
-                        name: `${cmds.length === 0 ? "In progress." : prefix + co.cname}`,
-                        value: co.des ? co.des : `No Description`,
-                        inline: true,
-                    }
-                    catts.push(dota)
-                });
-
-                cots.push(dir.toLowerCase());
-            });
-
-            const command =
-                client.commands.get(args[0].toLowerCase()) ||
-                client.commands.find(
-                    (c) => c.aliases && c.aliases.includes(args[0].toLowerCase())
-                );
-
-            if (cots.includes(args[0].toLowerCase())) {
-                const combed = new MessageEmbed()
-                    .setTitle(`__${args[0].charAt(0).toUpperCase() + args[0].slice(1)} Commands!__`)
-                    .setDescription(`Use \`${prefix}help\` followed by a command name to get more information on a command.\nFor example: \`${prefix}help ping\`.\n\n`)
-                    .addFields(catts)
-                    .setColor(color)
-
-                return message.channel.send({
-                    embeds: [combed]
-                })
-            };
-
-            if (!command) {
-                const embed = new MessageEmbed()
-                    .setTitle(`Invalid command! Use \`${prefix}help\` for all of my commands!`)
-                    .setColor("RED");
-                return await message.channel.send({
-                    embeds: [embed],
-                    allowedMentions: {
-                        repliedUser: false
-                    },
-                });
-            }
-
-            const embed = new MessageEmbed() 
-              .setTitle("Command Details:")
-                .addField(
-                    "Command:",
-                    command.name ? `\`${command.name}\`` : "No name for this command."
-                )
-                .addField(
-                    "Aliases:",
-                    command.aliases ?
-                    `\`${command.aliases.join("` `")}\`` :
-                    "No aliases for this command."
-                )
-                .addField(
-                    "Usage:",
-                    command.usage ?
-                    `\`${prefix}${command.name} ${command.usage}\`` :
-                    `\`${prefix}${command.name}\``
-                )
-                .addField(
-                    "Command Description:",
-                    command.description ?
-                    command.description :
-                    "No description for this command."
-                )
-                .setFooter(
-                    `Requested by ${message.author.tag}`,
-                    message.author.displayAvatarURL({
-                        dynamic: true
-                    })
-                )
-                .setTimestamp()
-                .setColor(color);
-            return await message.channel.send({
-                embeds: [embed]
-            });
+    const collector = m.createMessageComponentCollector({
+      filter: (b) => {
+      if(b.user.id === message.author.id) return true;
+       else {
+     b.reply({ ephemeral: true, content: `Only **${message.author.tag}** can use this button, if you want then you've to run the command again.`}); return false;
+           };
+      },
+      time : 200000
+    });
+    collector.on('collect', async (b) => {
+        if(b.customId === "home") {
+           if(!m) return;
+           return await m.edit({ embeds: [embed], components: [new MessageActionRow().addComponents(but1, but2, but3, but4)] })
         }
-    },
-};
+        if(b.customId === "music") {
+         _commands = client.commands.filter((x) => x.category && x.category === "Music").map((x) => `\`${x.name}\``);
+             editEmbed.setColor(client.embedColor).setDescription(_commands.join(", ")).setTitle("Music Commands").setFooter(`Total ${_commands.length} music commands.`);
+           if(!m) return;
+           return await m.edit({ embeds: [editEmbed], components: [new MessageActionRow().addComponents(but1, but2, but3, but4)] })
+        }
+         if(b.customId == "info") {
+         _commands = client.commands.filter((x) => x.category && x.category === "Information").map((x) => `\`${x.name}\``);
+             editEmbed.setColor(client.embedColor).setDescription(_commands.join(", ")).setTitle("Information Commands").setFooter(`Total ${_commands.length} Information commands.`)
+          return await m.edit({ embeds: [editEmbed], components: [new MessageActionRow().addComponents(but1, but2, but3, but4)] })
+         }
+         if(b.customId == "config") {
+         _commands = client.commands.filter((x) => x.category && x.category === "Config").map((x) => `\`${x.name}\``);
+             editEmbed.setColor(client.embedColor).setDescription(_commands.join(", ")).setTitle("Config Commands").setFooter(`Total ${_commands.length} Config commands.`)
+          return await m.edit({ embeds: [editEmbed], components: [new MessageActionRow().addComponents(but1, but2, but3, but4)] })
+         
+        }
+     });
+   }
+ }
