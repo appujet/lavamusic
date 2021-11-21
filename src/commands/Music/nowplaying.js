33c5ed4b1ed77894d71cad68e40264049bbaa3ec
S Dip <77108939+brblacky@users.noreply.github.com>
2021-11-21 08:02:10 +0530

    Update nowplaying.js

--- src/commands/Music/nowplaying.js
+++ src/commands/Music/nowplaying.js
@@ -3,7 +3,7 @@ const { convertTime } = require('../../utils/convert.js');
 const { progressbar } = require('../../utils/progressbar.js')
 
 module.exports = {
-  	name: "nowplaying",
+    name: "nowplaying",
     aliases: ["np"],
     category: "Music",
     description: "Show now playing song",
@@ -14,7 +14,7 @@ module.exports = {
     player: true,
     inVoiceChannel: false,
     sameVoiceChannel: false,
-	 execute: async (message, args, client, prefix) => {
+execute: async (message, args, client, prefix) => {
   
         const player = message.client.manager.get(message.guild.id);
 
@@ -24,18 +24,27 @@ module.exports = {
                 .setDescription("There is no music playing.");
             return message.channel.send(thing);
         }
-
         const song = player.queue.current
         const emojimusic = client.emoji.music;
         var total = song.duration;
         var current = player.position;
-
+        
         let embed = new MessageEmbed()
             .setDescription(`${emojimusic} **Now Playing**\n[${song.title}](${song.uri}) - \`[${convertTime(song.duration)}]\`- [${song.requester}] \n\n\`${progressbar(player)}\``)
             .setThumbnail(song.displayThumbnail("3"))
             .setColor(client.embedColor)
             .addField("\u200b", `\`${convertTime(current)} / ${convertTime(total)}\``)
-         return message.channel.send({embeds: [embed]})
-            
+         message.channel.send({embeds: [embed]}).then(message => player.set('nowplayingMSG', message))
+         
+          const interval = setInterval(() => {
+             let embed = new MessageEmbed()
+            .setDescription(`${emojimusic} **Now Playing**\n[${song.title}](${song.uri}) - \`[${convertTime(song.duration)}]\`- [${song.requester}] \n\n\`${progressbar(player)}\``)
+            .setThumbnail(song.displayThumbnail("3"))
+            .setColor(client.embedColor)
+            .addField("\u200b", `\`${convertTime(current)} / ${convertTime(total)}\``) 
+            player?.get('nowplayingMSG') ? player.get('nowplayingMSG').edit({embeds: [embed]}, "") : message.channel.send({embeds: [embed]}).then(message => player.set('nowplayingMSG', message))
+        }, 5000);
+        player.set('nowplaying', interval)
+      
     }
-};
+}
