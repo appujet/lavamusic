64d041de31d669e67faacf43a819e429f49c0cfa
S Dip <77108939+brblacky@users.noreply.github.com>
2021-11-28 10:58:08 +0530

    Update join.js

--- src/slashCommands/Music/join.js
+++ src/slashCommands/Music/join.js
@@ -2,7 +2,7 @@ const { MessageEmbed, CommandInteraction, Client } = require("discord.js")
 
 module.exports = {
   name: "join",
-  description: "Join voice channel"
+  description: "Join voice channel",
   /**
    * 
    * @param {Client} client 
