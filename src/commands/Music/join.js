e1364da7d486de42da62f62e5c05ef272bf7dcb6
S Dip <77108939+brblacky@users.noreply.github.com>
2021-11-30 14:21:27 +0530

    Update join.js

--- src/commands/Music/join.js
+++ src/commands/Music/join.js
@@ -1,47 +1,49 @@
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed, Permissions } = require("discord.js");
 
 module.exports = {
-	name: "join",
-    aliases: ["j"],
-    category: "Music",
-    description: "Join voice channel",
-    args: false,
-    usage: "",
-    permission: [],
-    owner: false,
-    player: false,
-    inVoiceChannel: true,
-    sameVoiceChannel: false,
- execute: async (message, args, client, prefix) => {
-  
-	    	const { channel } = message.member.voice;
+  name: "join",
+  aliases: ["j"],
+  category: "Music",
+  description: "Join voice channel",
+  args: false,
+  usage: "",
+  permission: [],
+  owner: false,
+  player: false,
+  inVoiceChannel: true,
+  sameVoiceChannel: false,
+  execute: async (message, args, client, prefix) => {
 
-        const emojiJoin = message.client.emoji.join;
+    if (!message.guild.me.permissions.has([Permissions.FLAGS.CONNECT, Permissions.FLAGS.SPEAK])) return message.channel.send({embeds: [new MessageEmbed().setColor(client.embedColor).setDescription(`I don't have enough permissions to execute this command.`)]});
+    
+    const { channel } = message.member.voice;
 
-        if(!message.guild.me.voice.channel) {
-            
-            const player = message.client.manager.create({
-                guild: message.guild.id,
-                voiceChannel: channel.id,
-                textChannel: message.channel.id,
-                volume: 80,
-                selfDeafen: true,
-            });
+    const emojiJoin = message.client.emoji.join;
 
-            player.connect();
+    if (!message.guild.me.voice.channel) {
 
-            let thing = new MessageEmbed()
-                .setColor(client.embedColor)
-                .setDescription(`${emojiJoin} **Join the voice channel**\nJoined <#${channel.id}> and bound to <#${message.channel.id}>`)
-             return message.reply({embeds: [thing]});
+      const player = message.client.manager.create({
+        guild: message.guild.id,
+        voiceChannel: channel.id,
+        textChannel: message.channel.id,
+        volume: 80,
+        selfDeafen: true,
+      });
 
-        } else if (message.guild.me.voice.channel !== channel) {
+      player.connect();
 
-            let thing = new MessageEmbed()
-                .setColor("RED")
-                .setDescription(`You must be in the same channel as ${message.client.user}`);
-            return message.reply({embeds: [thing]});
-        }
-        
+      let thing = new MessageEmbed()
+        .setColor(client.embedColor)
+        .setDescription(`${emojiJoin} **Join the voice channel**\nJoined <#${channel.id}> and bound to <#${message.channel.id}>`)
+      return message.reply({ embeds: [thing] });
+
+    } else if (message.guild.me.voice.channel !== channel) {
+
+      let thing = new MessageEmbed()
+        .setColor("RED")
+        .setDescription(`You must be in the same channel as ${message.client.user}`);
+      return message.reply({ embeds: [thing] });
     }
+
+  }
 };
