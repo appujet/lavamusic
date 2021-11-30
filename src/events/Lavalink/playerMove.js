9af279be486c517c1c36abccf6511c119b657f50
S Dip <77108939+brblacky@users.noreply.github.com>
2021-11-30 14:06:40 +0530

    Update playerMove.js

--- src/events/Lavalink/playerMove.js
+++ src/events/Lavalink/playerMove.js
@@ -1,9 +1,10 @@
 const { MessageEmbed } = require("discord.js");
 
 module.exports = async (client, player, oldChannel, newChannel) => {
-      if (!newChannel) {
+      const channel = guild.channels.cache.get(player.textChannel);
+       if (!newChannel) {
         await player.destroy();
-        return message.channel.send({ embeds: [new MessageEmbed()].setDescription("Music stopped as I have been disconnected from the voice channel.")})
+        return channel.send({ embeds: [new MessageEmbed()].setDescription("Music stopped as I have been disconnected from the voice channel.")})
       } else {
         player.voiceChannel = newChannel;
       }
