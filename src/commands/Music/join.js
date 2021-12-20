23b0af5b5e76935cba98580f8051d98367532c41
Blacky <77108939+brblacky@users.noreply.github.com>
2021-12-20 08:35:54 +0530

    Update join.js

--- src/commands/Music/join.js
+++ src/commands/Music/join.js
@@ -13,7 +13,11 @@ module.exports = {
   inVoiceChannel: true,
   sameVoiceChannel: false,
   execute: async (message, args, client, prefix) => {
-
+    
+    let player = message.client.manager.get(message.guildId);
+        if(player && player.voiceChannel && player.state === "CONNECTED") {
+            return await message.channel.send({embeds: [new MessageEmbed().setColor(client.embedColor).setDescription( `I'm already connected to <#${player.voiceChannel}> voice channel!`)]})
+        } else {
     if (!message.guild.me.permissions.has([Permissions.FLAGS.CONNECT, Permissions.FLAGS.SPEAK])) return message.channel.send({embeds: [new MessageEmbed().setColor(client.embedColor).setDescription(`I don't have enough permissions to execute this command! please give me permission \`CONNECT\` or \`SPEAK\`.`)]});
     
     const { channel } = message.member.voice;
@@ -22,30 +26,20 @@ module.exports = {
    
     const emojiJoin = message.client.emoji.join;
 
-    if (!message.guild.me.voice.channel) {
-
-      const player = message.client.manager.create({
+     player = message.client.manager.create({
         guild: message.guild.id,
         voiceChannel: channel.id,
         textChannel: message.channel.id,
         volume: 80,
         selfDeafen: true,
-      });
-
-      player.connect();
+      }) 
+      if(player && player.state !== "CONNECTED") player.connect();
 
       let thing = new MessageEmbed()
         .setColor(client.embedColor)
         .setDescription(`${emojiJoin} **Join the voice channel**\nJoined <#${channel.id}> and bound to <#${message.channel.id}>`)
       return message.reply({ embeds: [thing] });
 
-    } else if (message.guild.me.voice.channel !== channel) {
-
-      let thing = new MessageEmbed()
-        .setColor("RED")
-        .setDescription(`You must be in the same channel as ${message.client.user}`);
-      return message.reply({ embeds: [thing] });
-    }
-
+    };
   }
 };
