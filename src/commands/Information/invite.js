7773388ebb48b8c5eabb597f296ab16e123022c9
brblacky <sdipedit@gmail.com>
2022-01-03 08:22:48 +0000

    update new 1.0.5

--- src/commands/Information/invite.js
+++ src/commands/Information/invite.js
@@ -14,11 +14,11 @@ module.exports = {
          
     const row = new MessageActionRow()
 			.addComponents(
-        new MessageButton()
+     new MessageButton()
     .setLabel("Invite")
     .setStyle("LINK")
     .setURL(`https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=36768832&scope=applications.commands%20bot`),
-			new MessageButton()
+		new MessageButton()
     .setLabel("GitHub")
     .setStyle("LINK")
     .setURL("https://github.com/brblacky/lavamusic"),
@@ -29,7 +29,7 @@ module.exports = {
 			);
 
           const mainPage = new MessageEmbed()
-            .setAuthor({ text: 'LavaMusic', iconURL: 'https://media.discordapp.net/attachments/845318824323448882/876690332333514752/1629089649835.png'})
+            .setAuthor({ name: 'LavaMusic', iconURL: 'https://media.discordapp.net/attachments/845318824323448882/876690332333514752/1629089649835.png'})
             .setThumbnail('https://media.discordapp.net/attachments/845318824323448882/876690332333514752/1629089649835.png')
             .setColor('#303236')
             .addField('invite lavamusic', `[Here](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=36768832&scope=applications.commands%20bot)`, true)
