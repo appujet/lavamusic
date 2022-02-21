10c772dcc49a4b3250be0913b71bc4c8defa67f4
brblacky <sdipedit@gmail.com>
2022-02-21 09:46:06 +0000

    fixed playlist create command

--- src/commands/Playlist/create.js
+++ src/commands/Playlist/create.js
@@ -11,22 +11,14 @@ module.exports = {
     usage: i18n.__("cmd.playlist.create.use"),
     permission: [],
     owner: false,
-    player: true,
-    inVoiceChannel: true,
-    sameVoiceChannel: true,
+    player: false,
+    inVoiceChannel: false,
+    sameVoiceChannel: false,
     execute: async (message, args, client, prefix) => {
 
-        const player = message.client.manager.get(message.guild.id);
-        if (!player.queue.current) {
-            let thing = new MessageEmbed()
-                .setColor("RED")
-                .setDescription(i18n.__("player.nomusic"));
-            return message.reply({ embeds: [thing] });
-        }
         const Name = args[0];
         if (Name.length > 10) {
             return message.reply({ embeds: [new MessageEmbed().setColor(client.embedColor).setDescription(i18n.__("cmd.playlist.create.argsembed"))] });
-
         };
         let data = await db.find({
             UserId: message.author.id,
@@ -42,33 +34,11 @@ module.exports = {
         if (userData.length >= 10) {
             return message.reply({ embeds: [new MessageEmbed().setColor(client.embedColor).setDescription(i18n.__("cmd.playlist.create.existembed"))] })
         }
-        const song = player.queue.current;
-        const tracks = player.queue;
 
-        let oldSong = data.Playlist;
-        if (!Array.isArray(oldSong)) oldSong = [];
-        const newSong = [];
-        if (player.queue.current) {
-            newSong.push({
-                "title": song.title,
-                "uri": song.uri,
-                "author": song.author,
-                "duration": song.duration
-            });
-        }
-        for (const track of tracks)
-            newSong.push({
-                "title": track.title,
-                "uri": track.uri,
-                "author": track.author,
-                "duration": track.duration
-            });
-        const playlist = oldSong.concat(newSong);
         const newData = new db({
             UserName: message.author.tag,
             UserId: message.author.id,
             PlaylistName: Name,
-            Playlist: playlist,
             CreatedOn: Math.round(Date.now() / 1000)
         });
         await newData.save();
--- src/slashCommands/Playlist/create.js
+++ src/slashCommands/Playlist/create.js
@@ -29,19 +29,10 @@ module.exports = {
         const Name = interaction.options.getString("name");
         const data = await db.find({ UserId: interaction.member.user.id, PlaylistName: Name });
 
-        const player = client.manager.players.get(interaction.guildId);
-        if (!player.queue.current) {
-            let thing = new MessageEmbed()
-                .setColor("RED")
-                .setDescription(i18n.__("player.nomusic"));
-            return interaction.editReply({ embeds: [thing] });
-        }
         if (Name.length > 10) {
             return interaction.editReply({ embeds: [new MessageEmbed().setColor(client.embedColor).setDescription(i18n.__("cmd.playlist.create.argsembed"))] });
 
         };
-        
-
         if (data.length > 0) {
             return interaction.editReply({ embeds: [new MessageEmbed().setColor(client.embedColor).setDescription(i18n.__mf("cmd.playlist.create.dataembed", { prefix: prefix, name: Name }))] })
         };
@@ -51,41 +42,17 @@ module.exports = {
         if (userData.length >= 10) {
             return interaction.editReply({ embeds: [new MessageEmbed().setColor(client.embedColor).setDescription(i18n.__("cmd.playlist.create.existembed"))] })
         }
-        const song = player.queue.current;
-        const tracks = player.queue;
-
-        let oldSong = data.Playlist;
-        if (!Array.isArray(oldSong)) oldSong = [];
-        const newSong = [];
-        if (player.queue.current) {
-            newSong.push({
-                "title": song.title,
-                "uri": song.uri,
-                "author": song.author,
-                "duration": song.duration
-            });
-        }
-
-        for (const track of tracks)
-        newSong.push({
-            "title": track.title,
-            "uri": track.uri,
-            "author": track.author,
-            "duration": track.duration
+        const newData = new db({
+            UserName: interaction.user.tag,
+            UserId: interaction.user.id,
+            PlaylistName: Name,
+            CreatedOn: Math.round(Date.now() / 1000)
         });
-    const playlist = oldSong.concat(newSong);
-    const newData = new db({
-        UserName: interaction.user.tag,
-        UserId: interaction.user.id,
-        PlaylistName: Name,
-        Playlist: playlist,
-        CreatedOn: Math.round(Date.now() / 1000)
-    });
-    await newData.save();
-    const embed = new MessageEmbed()
-        .setDescription(i18n.__mf("cmd.playlist.create.mainembed", { name: Name }))
-        .setColor(client.embedColor)
-    return interaction.editReply({ embeds: [embed] })
+        await newData.save();
+        const embed = new MessageEmbed()
+            .setDescription(i18n.__mf("cmd.playlist.create.mainembed", { name: Name }))
+            .setColor(client.embedColor)
+        return interaction.editReply({ embeds: [embed] })
 
-}
+    }
 };
