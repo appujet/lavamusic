9c5a0c752c2686e44553d016f7404bddd64b943d
Amir-78 <amiramir.aa78m@gmail.com>
2022-05-27 19:06:20 +0100

    Client Users & Interaction Param & Interaction Reply fixed!

--- src/events/Client/interactionCreate.js
+++ src/events/Client/interactionCreate.js
@@ -1,4 +1,4 @@
-const { MessageEmbed, Client, Permissions } = require("discord.js")
+const { CommandInteraction, Client, Permissions } = require("discord.js")
 const pre = require("../../schema/prefix.js");
 const db2 = require("../../schema/dj");
 
@@ -10,34 +10,50 @@ module.exports = {
  * @param {CommandInteraction} interaction 
  */
     run: async (client, interaction) => {
-
         let prefix = client.prefix;
         const ress = await pre.findOne({ Guild: interaction.guildId })
         if (ress && ress.Prefix) prefix = ress.Prefix;
 
         if (interaction.isCommand() || interaction.isContextMenu()) {
-
             const SlashCommands = client.slashCommands.get(interaction.commandName);
             if (!SlashCommands) return;
 
             if (SlashCommands.owner && interaction.author.id !== `${client.owner}`) {
-                await interaction.editReply({
-                    content: `Only <@491577179495333903> can use this command!`
-                }).catch(() => { });
+                if (interaction.replied) {
+                    return await interaction.editReply({
+                        content: `Only <@491577179495333903> can use this command!`
+                    }).catch(() => { });
+                } else {
+                    return await interaction.reply({
+                        content: `Only <@491577179495333903> can use this command!`
+                    }).catch(() => { });
+                }
             }
             if (!interaction.member.permissions.has(SlashCommands.permissions || [])) {
                 return await interaction.reply({ content: `You need this \`${SlashCommands.permissions.join(", ")}\` permission to use this command `, ephemeral: true })
             }
             const player = interaction.client.manager.get(interaction.guildId);
             if (SlashCommands.player && !player) {
-                return await interaction.editReply({
-                    content: `There is no player for this guild.`, ephemeral: true
-                }).catch(() => { });
+                if (interaction.replied) {
+                    return await interaction.editReply({
+                        content: `There is no player for this guild.`, ephemeral: true
+                    }).catch(() => { });
+                } else {
+                    return await interaction.reply({
+                        content: `There is no player for this guild.`, ephemeral: true
+                    }).catch(() => { });
+                }
             }
             if (SlashCommands.inVoiceChannel && !interaction.member.voice.channel) {
-                return await interaction.editReply({
-                    content: `You must be in a voice channel!`, ephemeral: true
-                }).catch(() => { });
+                if (interaction.replied) {
+                    return await interaction.editReply({
+                        content: `You must be in a voice channel!`, ephemeral: true
+                    }).catch(() => { });
+                } else {
+                    return await interaction.reply({
+                        content: `You must be in a voice channel!`, ephemeral: true
+                    }).catch(() => { });
+                }
             }
             if (SlashCommands.sameVoiceChannel) {
                 if (interaction.guild.me.voice.channel) {
@@ -73,7 +89,7 @@ module.exports = {
                         content: `An unexcepted error occured.`
                     }).catch(() => { });
                 } else {
-                    await interaction.followUp({
+                    await interaction.reply({
                         ephemeral: true,
                         content: `An unexcepted error occured.`
                     }).catch(() => { });
--- src/events/Client/ready.js
+++ src/events/Client/ready.js
@@ -5,7 +5,7 @@ name: "ready",
 run: async (client) => {
     client.manager.init(client.user.id);
     client.logger.log(`${client.user.username} online!`, "ready");
-    client.logger.log(`Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`, "ready");
+    client.logger.log(`Ready on ${client.guilds.cache.size} servers, for a total of ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} users`, "ready");
 
     //Game
     let statuses = ['/help', `Prefix ${prefix}`];
