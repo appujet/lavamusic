07e972aeed12bcb18b81365a3b82ca7eb3c4d997
Anonymous4078 <95581967+Anonymous4078@users.noreply.github.com>
2022-07-14 16:39:10 +0530

    Update trackStuck.js
    Signed-off-by: Anonymous4078 <95581967+Anonymous4078@users.noreply.github.com>

--- src/events/Lavalink/trackStuck.js
+++ src/events/Lavalink/trackStuck.js
@@ -4,7 +4,7 @@ module.exports = async (client, player, track, payload) => {
     
     const channel = client.channels.cache.get(player.textChannel);
     const thing = new EmbedBuilder()
-        .setColor("RED")
+        .setColor("Red")
         .setDescription("❌ Error when loading song! Track is stuck");
     channel.send({embeds: [thing]}).then(msg => { setTimeout(() => { msg.delete() }, 5000) });
     client.logger.log(`Error when loading song! Track is stuck in [${player.guild}]`, "error");
