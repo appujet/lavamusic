4d5a889cfee9a97d2690ff876cfba8f4c1293df8
Anonymous4078 <95581967+Anonymous4078@users.noreply.github.com>
2022-07-22 09:05:42 +0530

    Update invite.js
    Signed-off-by: Anonymous4078 <95581967+Anonymous4078@users.noreply.github.com>

--- src/commands/Information/invite.js
+++ src/commands/Information/invite.js
@@ -31,7 +31,7 @@ module.exports = {
     const mainPage = new EmbedBuilder()
       .setAuthor({ name: 'LavaMusic', iconURL: 'https://media.discordapp.net/attachments/876035356460462090/888434725235097610/20210820_124325.png' })
       .setThumbnail('https://media.discordapp.net/attachments/876035356460462090/888434725235097610/20210820_124325.png')
-      .setColor('#303236')
+      .setColor(0x303236)
       .addFields([{ name: 'invite lavamusic', value: `[Here](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=36768832&scope=applications.commands%20bot)` }])
     message.reply({ embeds: [mainPage], components: [row] })
   }
