73ed246ce14d08e4d2fcdaf8d2ff56020161194d
NotAditya <90441096+NotAditya69@users.noreply.github.com>
2022-09-10 18:41:43 +0530

    Create config.js
    Signed-off-by: NotAditya <90441096+NotAditya69@users.noreply.github.com>

---
+++ src/config.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+
+module.exports = {
+    token: process.env.TOKEN || "",  // your bot token
+    clientID: process.env.CLIENT_ID || "", // your bot client id
+    prefix: process.env.PREFIX || "!", // bot prefix
+    ownerID: process.env.OWNERID || "", //your discord id
+    SpotifyID: process.env.SPOTIFYID || "",
+    SpotifySecret: process.env.SPOTIFYSECRET || "",
+    mongourl: process.env.MONGO_URI || "", // MongoDb URL
+    embedColor: process.env.COlOR || 0x303236, // embed colour
+    logs: process.env.LOGS || "875254787191504947", // channel id for guild create and delete logs
+    links: {
+        img: process.env.IMG || 'https://media.discordapp.net/attachments/963097935820750878/983300268131225651/20220606_145403.png', //setup system background image 
+        support: process.env.SUPPORT || 'https://discord.gg/ns8CTk9J3e', //support server invite link
+        invite: process.env.INVITE || 'https://discord.com/oauth2/authorize?client_id=977742811132743762&permissions=8&scope=bot%20applications.commands' //bot invite link
+    },
+    nodes: [
+        {
+            host: process.env.NODE_HOST || "54.37.6.86",
+            port: parseInt(process.env.NODE_PORT || "80"),
+            password: process.env.NODE_PASSWORD || "Blacky#9125",
+            secure: parseBoolean(process.env.NODE_SECURE || "false"),
+
+        }
+    ],
+
+}
+
+function parseBoolean(value) {
+    if (typeof (value) === 'string') {
+        value = value.trim().toLowerCase();
+    }
+    switch (value) {
+        case true:
+        case "true":
+            return true;
+        default:
+            return false;
+    }
+}
