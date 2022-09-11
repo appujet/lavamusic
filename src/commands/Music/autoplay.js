9209cf364ac7d7d816eac966e7cdca4aa26905a2
NotAditya69 <90441096+NotAditya69@users.noreply.github.com>
2022-09-11 10:48:17 +0530

    fix: autoplay doesn't works 🐛

--- src/commands/Music/autoplay.js
+++ src/commands/Music/autoplay.js
@@ -13,38 +13,38 @@ module.exports = {
   inVoiceChannel: true,
   sameVoiceChannel: true,
   execute: async (message, args, client, prefix) => {
-
     const player = client.manager.get(message.guild.id);
 
     const autoplay = player.get("autoplay");
 
     const emojireplay = client.emoji.autoplay;
-    
+
     if (!player.queue.current)
       return message.reply({
         content: `Please play a song before using this command.`,
       });
-    
-    if (autoplay === false) {
-      const identifier = player.queue.current.identifier;
-      player.set("autoplay", true);
-      player.set("requester", message.author);
-      player.set("identifier", identifier);
-      const search = `https://www.youtube.com/watch?v=${identifier}&list=RD${identifier}`;
-      res = await player.search(search, message.author);
-      player.queue.add(res.tracks[1]);
-      let thing = new EmbedBuilder()
-        .setColor(client.embedColor)
-        .setTimestamp()
-        .setDescription(`${emojireplay} Autoplay is now **enabled**.`);
-      return message.channel.send({ embeds: [thing] });
-    } else {
+
+    if (autoplay) {
       player.set("autoplay", false);
-      player.queue.clear();
       let thing = new EmbedBuilder()
         .setColor(client.embedColor)
         .setTimestamp()
         .setDescription(`${emojireplay} Autoplay is now **disabled**.`);
+      return message.channel.send({ embeds: [thing] });
+    } else {
+      const identifier = player.queue.current.identifier;
+      player.set("autoplay", true);
+      player.set("requester", client.user);
+      player.set("identifier", identifier);
+      const search = `https://www.youtube.com/watch?v=${identifier}&list=RD${identifier}`;
+      const res = await player.search(search, message.author);
+      player.queue.add(
+        res.tracks[Math.floor(Math.random() * res.tracks.length) ?? 1]
+      );
+      let thing = new EmbedBuilder()
+        .setColor(client.embedColor)
+        .setTimestamp()
+        .setDescription(`${emojireplay} Autoplay is now **enabled**.`);
 
       return message.channel.send({ embeds: [thing] });
     }
