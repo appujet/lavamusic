55bc58faa1bdb69e7309ea00829009ee99589be1
erwinowak <erwin1892@gmail.com>
2022-10-18 15:18:46 +0200

    Fix Args setPrefix

--- src/commands/Config/setprefix.js
+++ src/commands/Config/setprefix.js
@@ -12,43 +12,42 @@ module.exports = {
   execute: async (message, args, client, prefix) => {
 
     const data = await db.findOne({ Guild: message.guildId });
-    const pre = await args.join(" ")
-    if (!pre[0]) {
+    if (!args[0]) {
       const embed = new EmbedBuilder()
         .setDescription("Please provide the new prefix to set!")
         .setColor(client.embedColor)
       return message.reply({ embeds: [embed] });
     }
-    if (pre[1]) {
+    if (args[1]) {
       const embed = new EmbedBuilder()
         .setDescription("You can't set a prefix with a double argument!")
         .setColor(client.embedColor)
       return message.reply({ embeds: [embed] });
     }
-    if (pre[0].length > 3) {
+    if (args[0].length > 2) {
       const embed = new EmbedBuilder()
-        .setDescription("You can't set a prefix with more than 3 characters!")
+        .setDescription("You can't set a prefix with more than 2 characters!")
         .setColor(client.embedColor)
       return message.reply({ embeds: [embed] });
     }
     if (data) {
       data.oldPrefix = prefix;
-      data.Prefix = pre;
+      data.Prefix = args;
       await data.save()
       const update = new EmbedBuilder()
-        .setDescription(`Your prefix is being updated to **${pre}**`)
+        .setDescription(`Your prefix is being updated to **${args}**`)
         .setColor(client.embedColor)
         .setTimestamp()
       return message.reply({ embeds: [update] });
     } else {
       const newData = new db({
         Guild: message.guildId,
-        Prefix: pre,
+        Prefix: args,
         oldPrefix: prefix
       });
       await newData.save()
       const embed = new EmbedBuilder()
-        .setDescription(`The prefix has been successfully updated to **${pre}**`)
+        .setDescription(`The prefix has been successfully updated to **${args}**`)
         .setColor(client.embedColor)
         .setTimestamp()
       return message.reply({ embeds: [embed] });
