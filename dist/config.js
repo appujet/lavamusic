46314643fb22f020eff10904d69f2a4c029a3fd2
brblacky <sdipedit@gmail.com>
2023-03-18 20:36:45 -0700

    update v 3.1.0

--- dist/config.js
+++ dist/config.js
@@ -11,7 +11,7 @@ export default {
         yellow: 0xffff00,
         main: 0x2F3136,
     },
-    searchEngine: process.env.SEARCH_ENGINE || SearchEngine.SoundCloud,
+    searchEngine: process.env.SEARCH_ENGINE || SearchEngine.YouTube,
     maxPlaylistSize: parseInt(process.env.MAX_PLAYLIST_SIZE) || 100,
     maxQueueSize: parseInt(process.env.MAX_QUEUE_SIZE) || 100,
     owners: process.env.OWNERS?.split(','),
