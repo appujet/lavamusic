3cb40113be26cdddce597c330a19efb527ab7516
Ashesh <3626859+Ashesh3@users.noreply.github.com>
2023-05-09 21:28:47 +0530

    Move logo file load into exception handler
    Signed-off-by: Ashesh <3626859+Ashesh3@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,8 @@ if (!fs.existsSync("./src/utils/LavaLogo.txt")) {
   process.exit(1);
 }
 
-const logFile = fs.readFileSync("./src/utils/LavaLogo.txt", "utf-8");
 try {
+  const logFile = fs.readFileSync("./src/utils/LavaLogo.txt", "utf-8");
   console.log('\x1b[35m%s\x1b[0m', logFile);
 } catch (err) {
   logger.error("[CLIENT] An error has occurred :", err);
