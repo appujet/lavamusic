e13eae9c0cdf4f00b68705340635634e5afbdae5
Andrew Stoddard <acs8182000@gmail.com>
2024-02-25 18:18:19 -0500

    Added anisong and anilist checks

--- src/database/anime.ts
+++ src/database/anime.ts
@@ -1,5 +1,8 @@
 import Database from 'better-sqlite3';
 import ANNClient from '../structures/ANNClient.js';
+import AnisongClient, { AnisongData } from '../structures/AnisongClient.js';
+import AnilistClient from '../structures/AnilistClient.js';
+
 import config from '../config.js';
 
 const db = new Database('./database/anime.db', {
@@ -9,9 +12,13 @@ const db = new Database('./database/anime.db', {
 db.pragma('journal_mode=WAL');
 export default class AnimeData {
     private ANNClient: ANNClient
+    private AnisongClient: AnisongClient
+    private AnilistClient: AnilistClient
     constructor() {
         this.intialize();
         this.ANNClient = new ANNClient();
+        this.AnisongClient = new AnisongClient();
+        this.AnilistClient = new AnilistClient();
     }
     public intialize(): void {
         db.prepare(
@@ -46,26 +53,46 @@ export default class AnimeData {
                         this.addNewANN(element);
                     });
                 }
-                
             } , 1500)
         }
     }
     private async pullAllFromANN(): Promise<void> {
-        let allANNs = await this.ANNClient.getAllANNs()
-
+        let allANNs = await this.ANNClient.getAllANNs();
         allANNs.forEach(element => {
             this.addNewANN(element);
         });
     }
-    private addNewANN(ann: any): void {
-        //TODO: Check Anilist and Anisong
-        this.addAnnToDB(ann.id, ann.gid, ann.type, ann.name, ann.precision, false, false);
+    private async addNewANN(ann: any): Promise<void> {
+        let hasAnalist = false;
+        let hasAnisong = false;
+        let anisongData: AnisongData[];
+        let anilistId = 0;  
+        let type = ann.type.toLowerCase();        
+        if (type.includes('manga') || type.includes('anthology')) {
+            anilistId = await this.AnilistClient.SearchByMangaName(ann.name);
+        } else if (type.includes('tv') || type.includes('ona') || type.includes('ova') || type.includes('movie') || type.includes('special')) {
+            anilistId = await this.AnilistClient.SearchByAnimeName(ann.name);
+            anisongData = await this.AnisongClient.getAnisongDataFromANNId(ann.id);
+        } else {
+            console.log("Type of '" + ann.type + "' not found for annId '" + ann.id + "'. " + ann.name);
+        }
+        if (anilistId != 0) {
+            hasAnalist = true;
+            this.addAnilistMediaToDB(anilistId, ann.id);
+        }
+        if (anisongData.length > 0) {
+            hasAnisong = true;
+            anisongData.forEach(anisong => {
+                this.addAnisongToDB(anisong.anisongId, anisong.annId, anilistId, anisong.url, anisong.songType, anisong.anisongType, anisong.animeEng, anisong.animeJap, anisong.songName, anisong.animeType);
+            });
+        }
+        this.addAnnToDB(ann.id, ann.gid, ann.type.toLowerCase(), ann.name, ann.precision, hasAnalist, hasAnisong);
     }
     private addAnnToDB(annId: number, annGid: number, annType:string, annName:string, annPrecision:string, hasAnilist: boolean, hasAnisong: boolean): void {
         db.prepare('INSERT INTO ann (annId, gid, type, name, precision, hasAnilist, hasAnisong) VALUES(?, ?, ?, ?, ?, ?, ?)').run(annId, annGid, annType, annName, annPrecision, hasAnilist, hasAnisong);
     }
-    private addAnisongToDB(anisongId: number, annId: number, anilistMediaId: number, url: string, songType: string, anisongType: string, animeEng: string, animeJap: string, songName: string): void {
-        db.prepare('INSERT INTO ann (anisongId, annId, anilistMediaId, url, songType, anisongType, animeEng, animeJap, songName) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)').run(anisongId, annId, anilistMediaId, url, songType, anisongType, animeEng, animeJap, songName);
+    private addAnisongToDB(anisongId: number, annId: number, anilistMediaId: number, url: string, songType: string, anisongType: string, animeEng: string, animeJap: string, songName: string, animeType: string): void {
+        db.prepare('INSERT INTO ann (anisongId, annId, anilistMediaId, url, songType, anisongType, animeEng, animeJap, songName, animeType) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)').run(anisongId, annId, anilistMediaId, url, songType, anisongType, animeEng, animeJap, songName, animeType);
     }
     private addAnilistMediaToDB(anilistId: number, annId: number): void {
         db.prepare('INSERT INTO anilistmedia (anilistId, annId) VALUES(?, ?)').run(anilistId, annId);
