f9c234e385bfa183d7c8463d65cf13cee09d7b23
brblacky <77108939+brblacky@users.noreply.github.com>
2024-04-14 01:29:15 +0530

    Update package.json version to 4.0.9

--- src/events/player/NodeDestroy.ts
+++ src/events/player/NodeDestroy.ts
@@ -1,6 +1,8 @@
 import { Event, Lavamusic } from '../../structures/index.js';
 import BotLog from '../../utils/BotLog.js';
 
+let destroyCount = 0;
+
 export default class NodeDestroy extends Event {
     constructor(client: Lavamusic, file: string) {
         super(client, file, {
@@ -14,5 +16,16 @@ export default class NodeDestroy extends Event {
             `Node ${node} destroyed with code ${code} and reason ${reason}`,
             'error'
         );
+        destroyCount++;
+        if (destroyCount >= 5) {
+            this.client.shoukaku.removeNode(node);
+            destroyCount = 0;
+            this.client.logger.warn(`Node ${node} removed from nodes list due to excessive disconnects`);
+            BotLog.send(
+                this.client,
+                `Node ${node} removed from nodes list due to excessive disconnects`,
+                'warn'
+            );
+        }
     }
 }
