0dc67fecee066dd88820e77f6628f0663f0a9d38
brblacky <77108939+brblacky@users.noreply.github.com>
2024-04-14 02:08:37 +0530

    Refactor config.ts to use process.env variables for default values

--- src/config.ts
+++ src/config.ts
@@ -13,41 +13,37 @@ export default {
         yellow: 0xffff00,
         main: 0x2f3136,
     },
-    keepAlive: parseBoolean(process.env.KEEP_ALIVE) || false, // for https://replit.com keep alive bot 24/7
-    autoNode: parseBoolean(process.env.AUTO_NODE) || true, // auto node for lavamusic bot it is gave from lavainfo-api "https://lavainfo-api.freedback-dip.workers.dev"
-    searchEngine: process.env.SEARCH_ENGINE || (SearchEngine.YouTube as SearchEngine),
-    maxPlaylistSize: parseInt(process.env.MAX_PLAYLIST_SIZE) || 100,
+    keepAlive: process.env.KEEP_ALIVE ? parseBoolean(process.env.KEEP_ALIVE) : false, // for https://replit.com keep alive bot 24/7
+    autoNode: process.env.AUTO_NODE ? parseBoolean(process.env.AUTO_NODE) : true, // auto node for lavamusic bot it is gave from lavainfo-api "https://lavainfo-api.freedback-dip.workers.dev"
+    searchEngine: process.env.SEARCH_ENGINE || SearchEngine.YouTube, // search engine for music
+    maxPlaylistSize: process.env.MAX_PLAYLIST_SIZE ? parseInt(process.env.MAX_PLAYLIST_SIZE) : 100, // max playlist size
     botStatus: process.env.BOT_STATUS || 'online', // online, idle, dnd, invisible
     botActivity: process.env.BOT_ACTIVITY || 'Lavamusic', // set the bot activity
-    botActivityType: parseInt(process.env.BOT_ACTIVITY_TYPE || '2'), // 0 to 5 get more info - https://discord.com/developers/docs/topics/gateway-events#activity-object-activity-types
-    maxQueueSize: parseInt(process.env.MAX_QUEUE_SIZE) || 100,
-    owners: JSON.parse(process.env.OWNER_IDS || '[]'),
-    clientId: process.env.CLIENT_ID,
-    guildId: process.env.GUILD_ID,
-    logChannelId: process.env.LOG_CHANNEL_ID || '',
+    botActivityType: process.env.BOT_ACTIVITY_TYPE ? parseInt(process.env.BOT_ACTIVITY_TYPE) : 2, // 0 = PLAYING, 1 = STREAMING, 2 = LISTENING, 3 = WATCHING
+    maxQueueSize: process.env.MAX_QUEUE_SIZE ? parseInt(process.env.MAX_QUEUE_SIZE) : 100, // max queue size
+    owners: process.env.OWNER_IDS ? JSON.parse(process.env.OWNER_IDS) : [], // bot owners
+    clientId: process.env.CLIENT_ID, // bot client id
+    guildId: process.env.GUILD_ID, // guild id
+    logChannelId: process.env.LOG_CHANNEL_ID || '', // log channel id
     links: {
         img: process.env.IMG_LINK || 'https://i.imgur.com/ud3EWNh.jpg',
-    },
+    }, // image links
     icons: {
-        youtube:
-            'https://cdn.discordapp.com/attachments/852316384289619968/1142853793822822551/3670147.png',
-        spotify:
-            'https://media.discordapp.net/attachments/963097935820750878/1054333449252655104/spotify.png',
-        soundcloud:
-            'https://media.discordapp.net/attachments/963097935820750878/1054333449638526986/145809.png',
-        applemusic:
-            'https://media.discordapp.net/attachments/963097935820750878/1054333450368340018/apple-music-icon.png',
-        deezer: 'https://media.discordapp.net/attachments/963097935820750878/1054333450024394802/5968803.png',
-    },
-    production: parseBoolean(process.env.PRODUCTION) || true,
+        youtube: 'https://i.imgur.com/xzVHhFY.png',
+        spotify: 'https://i.imgur.com/qvdqtsc.png',
+        soundcloud: 'https://i.imgur.com/MVnJ7mj.png',
+        applemusic: 'https://i.imgur.com/Wi0oyYm.png',
+        deezer: 'https://i.imgur.com/xyZ43FG.png',
+    }, // music icons
+    production: process.env.PRODUCTION ? parseBoolean(process.env.PRODUCTION) : true,
     lavalink: [
         {
             url: process.env.LAVALINK_URL,
             auth: process.env.LAVALINK_AUTH,
             name: process.env.LAVALINK_NAME,
-            secure: parseBoolean(process.env.LAVALINK_SECURE) || false,
+            secure: process.env.LAVALINK_SECURE ? parseBoolean(process.env.LAVALINK_SECURE) : false,
         },
-    ],
+    ], // lavalink nodes
 };
 
 function parseBoolean(value: string | undefined): boolean {
