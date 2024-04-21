bde8ef75c829a5d71b2bf79c21159f922dc03368
Nhut Lam <105686900+nhutlamm@users.noreply.github.com>
2024-04-21 21:38:03 +0700

    update info watermarks

--- src/config.ts
+++ src/config.ts
@@ -59,9 +59,9 @@ function parseBoolean(value: string | undefined): boolean {
 }
 /**
  * Project: lavamusic
- * Author: Blacky
+ * Author: Appu
  * Company: Coders
- * Copyright (c) 2023. All rights reserved.
+ * Copyright (c) 2024. All rights reserved.
  * This code is the property of Coder and may not be reproduced or
  * modified without permission. For more information, contact us at
  * https://discord.gg/ns8CTk9J3e
--- src/events/client/InteractionCreate.ts
+++ src/events/client/InteractionCreate.ts
@@ -233,7 +233,7 @@ return interaction.reply({ content: `You can't use commands in setup channel.` ,
  * Project: lavamusic
  * user: Blacky
  * Company: Coders
- * Copyright (c) 2023. All rights reserved.
+ * Copyright (c) 2024. All rights reserved.
  * This code is the property of Coder and may not be reproduced or
  * modified without permission. For more information, contact us at
  * https://discord.gg/ns8CTk9J3e
