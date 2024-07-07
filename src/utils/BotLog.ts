65d551964a58f6b6b52b24997f7f819509590249
hwangsihu <hwangsihu27@gmail.com>
2024-07-07 23:12:47 +0900

    Fixed again

--- src/utils/BotLog.ts
+++ src/utils/BotLog.ts
@@ -1,15 +1,12 @@
 import type { TextChannel } from "discord.js";
-
 import type { Lavamusic } from "../structures/index.js";
 
 // biome-ignore lint/complexity/noStaticOnlyClass: <explanation>
 export default class BotLog {
     public static send(client: Lavamusic, message: string, type: "error" | "warn" | "info" | "success" = "info"): void {
         if (!client?.channels.cache && client.config.logChannelId) return;
-
         const channel = client.channels.cache.get(client.config.logChannelId) as TextChannel;
         if (!channel) return;
-
         const colors: { [key: string]: number } = {
             error: 0xff0000,
             warn: 0xffff00,
@@ -17,9 +14,7 @@ export default class BotLog {
             success: 0x00ff00,
         };
         const color = colors[type] || 0x000000;
-
         const embed = client.embed().setColor(color).setDescription(message).setTimestamp();
-
         channel.send({ embeds: [embed] }).catch(() => {});
     }
 }
