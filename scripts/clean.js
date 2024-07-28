24fc9ce3525a9d236bbee50478c3777744b780e3
hwangsihu <hwangsihu27@gmail.com>
2024-07-28 15:36:54 +0900

    Update

--- scripts/clean.js
+++ scripts/clean.js
@@ -3,14 +3,11 @@ import { rm } from "node:fs/promises";
 import { resolve } from "node:path";
 
 async function clean() {
-    console.log("Cleaning dist folder...");
     try {
         const path = resolve("dist");
         if (existsSync(path)) {
             await rm(path, { recursive: true, force: true });
-            console.log("Dist folder has been cleaned");
         } else {
-            console.log("Dist folder does not exist");
         }
     } catch (error) {
         console.error("Error while cleaning dist folder:", error);
