c3abeaf802b1af2cde6a81dd250020f8c64d9cbf
appujet <sdipedit@gmail.com>
2024-07-31 17:02:02 +0530

    chore: Update run.bat to check for pnpm installation and install dependencies if node_modules does not exist

--- run.bat
+++ run.bat
@@ -1,2 +1,19 @@
 @echo off
-npm start
+setlocal enabledelayedexpansion
+
+:: Check if pnpm is installed
+
+where pnpm >nul 2>nul
+if %errorlevel% equ 0 (
+    set package_manager=pnpm
+) else (
+    set package_manager=npm
+)
+
+:: Check if node_modules exists
+if not exist node_modules (
+    %package_manager% install
+)
+
+:: start the project
+%package_manager% run start
