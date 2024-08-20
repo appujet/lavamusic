205018d897baa08517e0080aa114ec59e87b2966
Aiko <159515125+AikooNee@users.noreply.github.com>
2024-08-20 11:03:07 +0530

    fix
    Signed-off-by: Aiko <159515125+AikooNee@users.noreply.github.com>

--- src/commands/dev/CreateInvite.ts
+++ src/commands/dev/CreateInvite.ts
@@ -30,7 +30,7 @@ export default class CreateInvite extends Command {
         });
     }
 
-    public async run(client: Lavamusic, ctx: Context, args: string[]): Promise<void> {
+    public async run(client: Lavamusic, ctx: Context, args: string[]): Promise<any> {
         const guildId = args[0];
         const guild = client.guilds.cache.get(guildId);
 
