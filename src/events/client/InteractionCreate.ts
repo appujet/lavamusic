5237a4b243d935993193bed2f58f8bd65a5721ad
hwangsihu <hwangsihu27@gmail.com>
2024-09-05 18:46:21 +0900

    Update packages, formatting, and fix i18n

--- src/events/client/InteractionCreate.ts
+++ src/events/client/InteractionCreate.ts
@@ -104,13 +104,13 @@ export default class InteractionCreate extends Event {
                 if (!voted) {
                     const voteBtn = new ActionRowBuilder<ButtonBuilder>().addComponents(
                         new ButtonBuilder()
-                            .setLabel("Vote for Me!")
+                            .setLabel(T(locale, "event.interaction.vote_button"))
                             .setURL(`https://top.gg/bot/${this.client.config.clientId}/vote`)
                             .setStyle(ButtonStyle.Link),
                     );
 
                     return await interaction.reply({
-                        content: "Wait! Before using this command, you must vote on top.gg. Thank you.",
+                        content: T(locale, "event.interaction.vote_message"),
                         components: [voteBtn],
                         ephemeral: true,
                     });
