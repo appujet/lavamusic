3290ab293c95f4514cadd1579357e2197534ffef
TheSkinnyRat <purwa.sabrang@gmail.com>
2024-12-18 01:29:19 +0700

    refactor: disable node error logging

--- src/events/node/Error.ts
+++ src/events/node/Error.ts
@@ -1,6 +1,6 @@
-import type { LavalinkNode } from 'lavalink-client';
+// import type { LavalinkNode } from 'lavalink-client';
 import { Event, type Lavamusic } from '../../structures/index';
-import { sendLog } from '../../utils/BotLog';
+// import { sendLog } from '../../utils/BotLog';
 
 export default class NodeError extends Event {
 	constructor(client: Lavamusic, file: string) {
@@ -9,8 +9,8 @@ export default class NodeError extends Event {
 		});
 	}
 
-	public async run(node: LavalinkNode, error: Error): Promise<void> {
-		// this.client.logger.error(`Node ${node.id} has an error: ${error.message}`);
-		sendLog(this.client, `Node Error: ${node.id}: ${error.message}`, 'error');
-	}
+	// public async run(node: LavalinkNode, error: Error): Promise<void> {
+	// 	// this.client.logger.error(`Node ${node.id} has an error: ${error.message}`);
+	// 	sendLog(this.client, `Node Error: ${node.id}: ${error.message}`, 'error');
+	// }
 }
