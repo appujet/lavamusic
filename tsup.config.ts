01b1d5739b54502e60a95bee4e75b625fb8a6bc8
appujet <sdipedit@gmail.com>
2025-03-17 21:43:40 +0530

    refactor package.json scripts, remove clean script, and add tsup configuration

---
+++ tsup.config.ts
@@ -0,0 +1,8 @@
+import { type Options, defineConfig } from "tsup";
+
+export default defineConfig((options: Options) => ({
+  entryPoints: ["src/**/*.ts"],
+  clean: true,
+  format: "cjs",
+  ...options,
+}));
