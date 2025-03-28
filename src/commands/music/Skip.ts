2e53dc9afc73c782239a9f58e5b905c5f3a38fe3
JustEvil <141197672+justeevil@users.noreply.github.com>
2025-03-28 16:20:12 +0530

    Force skip the song if autoplay mode is enabled
    Allowing to forcefully skip the currently playing song when autoplay mode is enabled
    
    Signed-off-by: JustEvil <141197672+justeevil@users.noreply.github.com>

--- src/commands/music/Skip.ts
+++ src/commands/music/Skip.ts
@@ -32,15 +32,16 @@ export default class Skip extends Command {
 
 	public async run(client: Lavamusic, ctx: Context): Promise<any> {
 		const player = client.manager.getPlayer(ctx.guild!.id);
-		const embed = this.client.embed();
+		const embed = this.client.embed();	
 		if (!player) return await ctx.sendMessage(ctx.locale('event.message.no_music_playing'));
-		if (player.queue.tracks.length === 0) {
+		const autoplay = player.get<boolean>('autoplay');
+		if (!autoplay && player.queue.tracks.length === 0) {
 			return await ctx.sendMessage({
 				embeds: [embed.setColor(this.client.color.red).setDescription(ctx.locale('player.errors.no_song'))],
 			});
 		}
 		const currentTrack = player.queue.current?.info;
-		player.skip();
+		player.skip(0, !autoplay);
 		if (ctx.isInteraction) {
 			return await ctx.sendMessage({
 				embeds: [
