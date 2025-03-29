2722df50173cd64eead2de5de9e66b72624be028
Shadow Gaming <81495140+ShadowGaming100@users.noreply.github.com>
2025-03-29 19:12:19 +0000

    Update Restart.ts
    Signed-off-by: Shadow Gaming <81495140+ShadowGaming100@users.noreply.github.com>

--- src/commands/dev/Restart.ts
+++ src/commands/dev/Restart.ts
@@ -98,3 +98,13 @@ export default class Restart extends Command {
 		});
 	}
 }
+/**
+ * Project: lavamusic
+ * Author: Appu
+ * Main Contributor: LucasB25
+ * Company: Coders
+ * Copyright (c) 2024. All rights reserved.
+ * This code is the property of Coder and may not be reproduced or
+ * modified without permission. For more information, contact us at
+ * https://discord.gg/YQsGbTwPBx
+ */
