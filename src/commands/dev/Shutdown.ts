12c2d044e5f799ef11de388a89a765c76e5f5e39
Shadow Gaming <81495140+ShadowGaming100@users.noreply.github.com>
2025-03-29 19:12:05 +0000

    Update Shutdown.ts
    Signed-off-by: Shadow Gaming <81495140+ShadowGaming100@users.noreply.github.com>

--- src/commands/dev/Shutdown.ts
+++ src/commands/dev/Shutdown.ts
@@ -76,3 +76,15 @@ export default class Shutdown extends Command {
 		});
 	}
 }
+
+
+/**
+ * Project: lavamusic
+ * Author: Appu
+ * Main Contributor: LucasB25
+ * Company: Coders
+ * Copyright (c) 2024. All rights reserved.
+ * This code is the property of Coder and may not be reproduced or
+ * modified without permission. For more information, contact us at
+ * https://discord.gg/YQsGbTwPBx
+ */
