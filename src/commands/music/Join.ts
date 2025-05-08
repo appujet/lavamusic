f101d802e8d9bbd3eb397be35f5bc5b2d96eaf4b
hwangsihu <129564966+hwangsihu@users.noreply.github.com>
2025-05-08 22:31:34 +0900

    Formatting

--- src/commands/music/Join.ts
+++ src/commands/music/Join.ts
@@ -1,78 +1,90 @@
-import type { VoiceChannel } from 'discord.js';
-import { Command, type Context, type Lavamusic } from '../../structures/index';
+import type { VoiceChannel } from "discord.js";
+import { Command, type Context, type Lavamusic } from "../../structures/index";
 
 export default class Join extends Command {
-	constructor(client: Lavamusic) {
-		super(client, {
-			name: 'join',
-			description: {
-				content: 'cmd.join.description',
-				examples: ['join'],
-				usage: 'join',
-			},
-			category: 'music',
-			aliases: ['come', 'j'],
-			cooldown: 3,
-			args: false,
-			vote: false,
-			player: {
-				voice: true,
-				dj: false,
-				active: false,
-				djPerm: null,
-			},
-			permissions: {
-				dev: false,
-				client: ['SendMessages', 'ReadMessageHistory', 'ViewChannel', 'EmbedLinks', 'Connect', 'Speak'],
-				user: [],
-			},
-			slashCommand: true,
-			options: [],
-		});
-	}
+  constructor(client: Lavamusic) {
+    super(client, {
+      name: "join",
+      description: {
+        content: "cmd.join.description",
+        examples: ["join"],
+        usage: "join",
+      },
+      category: "music",
+      aliases: ["come", "j"],
+      cooldown: 3,
+      args: false,
+      vote: false,
+      player: {
+        voice: true,
+        dj: false,
+        active: false,
+        djPerm: null,
+      },
+      permissions: {
+        dev: false,
+        client: [
+          "SendMessages",
+          "ReadMessageHistory",
+          "ViewChannel",
+          "EmbedLinks",
+          "Connect",
+          "Speak",
+        ],
+        user: [],
+      },
+      slashCommand: true,
+      options: [],
+    });
+  }
 
-	public async run(client: Lavamusic, ctx: Context): Promise<any> {
-		const embed = this.client.embed();
-		let player = client.manager.getPlayer(ctx.guild!.id);
+  public async run(client: Lavamusic, ctx: Context): Promise<any> {
+    const embed = this.client.embed();
+    let player = client.manager.getPlayer(ctx.guild!.id);
 
-		if (player) {
-			return await ctx.sendMessage({
-				embeds: [
-					embed.setColor(this.client.color.main).setDescription(
-						ctx.locale('cmd.join.already_connected', {
-							channelId: player.voiceChannelId,
-						}),
-					),
-				],
-			});
-		}
+    if (player) {
+      return await ctx.sendMessage({
+        embeds: [
+          embed.setColor(this.client.color.main).setDescription(
+            ctx.locale("cmd.join.already_connected", {
+              channelId: player.voiceChannelId,
+            }),
+          ),
+        ],
+      });
+    }
 
-		const memberVoiceChannel = (ctx.member as any).voice.channel as VoiceChannel;
-		if (!memberVoiceChannel) {
-			return await ctx.sendMessage({
-				embeds: [embed.setColor(this.client.color.red).setDescription(ctx.locale('cmd.join.no_voice_channel'))],
-			});
-		}
+    const memberVoiceChannel = (ctx.member as any).voice
+      .channel as VoiceChannel;
+    if (!memberVoiceChannel) {
+      return await ctx.sendMessage({
+        embeds: [
+          embed
+            .setColor(this.client.color.red)
+            .setDescription(ctx.locale("cmd.join.no_voice_channel")),
+        ],
+      });
+    }
 
-		player = client.manager.createPlayer({
-			guildId: ctx.guild!.id,
-			voiceChannelId: memberVoiceChannel.id,
-			textChannelId: ctx.channel.id,
-			selfMute: false,
-			selfDeaf: true,
-			vcRegion: memberVoiceChannel.rtcRegion!,
-		});
-		if (!player.connected) await player.connect();
-		return await ctx.sendMessage({
-			embeds: [
-				embed.setColor(this.client.color.main).setDescription(
-					ctx.locale('cmd.join.joined', {
-						channelId: player.voiceChannelId,
-					}),
-				),
-			],
-		});
-	}
+    player = client.manager.createPlayer({
+      guildId: ctx.guild!.id,
+      voiceChannelId: memberVoiceChannel.id,
+      textChannelId: ctx.channel.id,
+      selfMute: false,
+      selfDeaf: true,
+      vcRegion: memberVoiceChannel.rtcRegion!,
+    });
+    if (!player.connected) await player.connect();
+    return await ctx.sendMessage({
+      embeds: [
+        embed.setColor(this.client.color.main).setDescription(
+          ctx.locale("cmd.join.joined", {
+            channelId: player.voiceChannelId,
+          }),
+        ),
+      ],
+    });
+  }
 }
 
 /**
--- src/events/client/GuildCreate.ts
+++ src/events/client/GuildCreate.ts
@@ -1,74 +1,85 @@
-import { EmbedBuilder, type Guild, type GuildMember, type TextChannel } from 'discord.js';
-import { Event, type Lavamusic } from '../../structures/index';
+import {
+  EmbedBuilder,
+  type Guild,
+  type GuildMember,
+  type TextChannel,
+} from "discord.js";
+import { Event, type Lavamusic } from "../../structures/index";
 
 export default class GuildCreate extends Event {
-	constructor(client: Lavamusic, file: string) {
-		super(client, file, {
-			name: 'guildCreate',
-		});
-	}
+  constructor(client: Lavamusic, file: string) {
+    super(client, file, {
+      name: "guildCreate",
+    });
+  }
 
-	public async run(guild: Guild): Promise<void> {
-		let owner: GuildMember | undefined;
-		try {
-			owner = await guild.members.fetch(guild.ownerId);
-		} catch (e) {
-			this.client.logger.error(`Error fetching owner for guild ${guild.id}: ${e}`);
-		}
+  public async run(guild: Guild): Promise<void> {
+    let owner: GuildMember | undefined;
+    try {
+      owner = await guild.members.fetch(guild.ownerId);
+    } catch (e) {
+      this.client.logger.error(
+        `Error fetching owner for guild ${guild.id}: ${e}`,
+      );
+    }
 
-		const embed = new EmbedBuilder()
-			.setColor(this.client.config.color.green)
-			.setAuthor({
-				name: guild.name,
-				iconURL: guild.iconURL({ extension: 'jpeg' })!,
-			})
-			.setDescription(`**${guild.name}** has been added to my guilds!`)
-			.setThumbnail(guild.iconURL({ extension: 'jpeg' }))
-			.addFields(
-				{
-					name: 'Owner',
-					value: owner ? owner.user.tag : 'Unknown#0000',
-					inline: true,
-				},
-				{
-					name: 'Members',
-					value: guild.memberCount.toString(),
-					inline: true,
-				},
-				{
-					name: 'Created At',
-					value: `<t:${Math.floor(guild.createdTimestamp / 1000)}:F>`,
-					inline: true,
-				},
-				{
-					name: 'Joined At',
-					value: `<t:${Math.floor(guild.joinedTimestamp / 1000)}:F>`,
-					inline: true,
-				},
-				{ name: 'ID', value: guild.id, inline: true },
-			)
-			.setTimestamp();
+    const embed = new EmbedBuilder()
+      .setColor(this.client.config.color.green)
+      .setAuthor({
+        name: guild.name,
+        iconURL: guild.iconURL({ extension: "jpeg" })!,
+      })
+      .setDescription(`**${guild.name}** has been added to my guilds!`)
+      .setThumbnail(guild.iconURL({ extension: "jpeg" }))
+      .addFields(
+        {
+          name: "Owner",
+          value: owner ? owner.user.tag : "Unknown#0000",
+          inline: true,
+        },
+        {
+          name: "Members",
+          value: guild.memberCount.toString(),
+          inline: true,
+        },
+        {
+          name: "Created At",
+          value: `<t:${Math.floor(guild.createdTimestamp / 1000)}:F>`,
+          inline: true,
+        },
+        {
+          name: "Joined At",
+          value: `<t:${Math.floor(guild.joinedTimestamp / 1000)}:F>`,
+          inline: true,
+        },
+        { name: "ID", value: guild.id, inline: true },
+      )
+      .setTimestamp();
 
-		const logChannelId = this.client.env.LOG_CHANNEL_ID;
-		if (!logChannelId) {
-			this.client.logger.error('Log channel ID not found in configuration.');
-			return;
-		}
+    const logChannelId = this.client.env.LOG_CHANNEL_ID;
+    if (!logChannelId) {
+      this.client.logger.error("Log channel ID not found in configuration.");
+      return;
+    }
 
-		try {
-			const channel = (await this.client.channels.fetch(logChannelId)) as TextChannel;
-			if (!channel) {
-				this.client.logger.error(
-					`Log channel not found with ID ${logChannelId}. Please change the settings in .env or, if you have a channel, invite me to that guild.`,
-				);
-				return;
-			}
+    try {
+      const channel = (await this.client.channels.fetch(
+        logChannelId,
+      )) as TextChannel;
+      if (!channel) {
+        this.client.logger.error(
+          `Log channel not found with ID ${logChannelId}. Please change the settings in .env or, if you have a channel, invite me to that guild.`,
+        );
+        return;
+      }
 
-			await channel.send({ embeds: [embed] });
-		} catch (error) {
-			this.client.logger.error(`Error sending message to log channel ${logChannelId}: ${error}`);
-		}
-	}
+      await channel.send({ embeds: [embed] });
+    } catch (error) {
+      this.client.logger.error(
+        `Error sending message to log channel ${logChannelId}: ${error}`,
+      );
+    }
+  }
 }
 
 /**
